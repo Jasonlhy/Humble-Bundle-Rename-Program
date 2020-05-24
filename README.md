efedcaa3de30da8bb2b848f2724beab8333dd86e
jasonlhy <jasonlhy@gmail.com>
2020-05-24 22:29:43 +0800

    Add usage and document

---
+++ README.md
@@ -0,0 +1,36 @@
+# Purpose
+
+I bought some books from [Humble Bundle](https://www.humblebundle.com/) with PDF format, the default filenames are **ALWAYS** in unreadable format.
+ 
+- dimensionalanalysisforunitconversionusingmatlab.pdf
+- algebraessentials.pdf
+- multivariableandvectorcalculus.pdf
+
+
+Therefore, the purpose of this program is quite simple, it will rename the unreadable filename into a more human readable format by finding the largest word tokens that can pass the spell check, and combines them into the new filename. For example: 
+
+- computertheory.pdf -> computer-theory.pdf
+- linearalgebra.pdf -> linear-algebra.pdf
+- algebraessentials.pdf -> algebra-essentials.pdf
+
+## Requirements
+
+- Python 3
+- pip install cyhunspell
+
+## Usage
+
+```bash
+python rename.py
+Path of the folder: C:\\Users\\jason\\Documents\\RenamePDFs\\TestData
+Extension you want to rename to: .pdf
+Are you sure to rename the file under directory C:\\Users\\jason\\Documents\\RenamePDFs\\TestData with extensions .pdf [Y/y]: Y
+```
+
+## Why greedy algorithm?
+Because all followings are true
+
+- h.spell("a") => True
+- h.spell("algebra") => True
+- h.spell("essentials") => True
+- h.spell("essential") => True
--- rename.py
+++ rename.py
@@ -1,16 +1,21 @@
 import os
-import shutil
 from hunspell import Hunspell
 h = Hunspell()
 
-def spellvalid_path(path: str):
-    [filename, extension] = os.path.splitext(os.path.basename(path))
+def spellvalid_path(basename):
+    """Find out the most likely spell valid filename with greedy algorithm
+
+        For example:
+        - computertheory -> computer-theory
+        - linearalgebra -> linear-algebra
+        - algebraessentials -> algebra-essentials
+    """
+    [filename, extension] = os.path.splitext(basename)
 
     # Compute the largest spell valid substring start with every character
     # Store the (pattern, (startIdx, endIdx (inclusive)))
     large_tokens = []
-    end = len(filename)
-    i = 0
+    i, end = 0, len(filename)
     while i < end:
         # record the greedy information
         nextI = i + 1
@@ -24,31 +29,49 @@ def spellvalid_path(path: str):
 
         if greedy_token != None:
             large_tokens.append(greedy_token)
-        
+
         # If we find the spell greedy token, other next token will look after than token
         # Otherwise we increment by 1
         i = nextI
-        
 
-    print(large_tokens)
     valid_filename = "-".join(str(x.strip()) for x in large_tokens)
-    print("valid_filename: " + valid_filename)
+    return valid_filename + extension
 
-    filepath = os.path.join(os.path.dirname(path), valid_filename + extension)
-    return filepath
 
-def batch_rename_pdf(path: str):
-    filenames = [s for s in os.listdir(path) if s.endswith(".pdf")]
-    filenames = [os.path.join(path, s) for s in filenames]
+def batch_rename(path, extension=".pdf"):
+    """Given the path of the directory, batch rename the file names to be spell valid filename
+
+        For example:
+        - C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\computertheory.pdf -> C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\computer-theory.pdf
+        - C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\linearalgebra.pdf -> C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\linear-algebra.pdf
+        - C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\algebraessentials.pdf -> C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\algebra-essentials.pdf
+    """
+    filenames = [s for s in os.listdir(path) if s.endswith(extension)]
 
     # Tuple of (filepath, valid spell filepath)
+    # batch_filename_tuples = [(s, os.path.join(path, spellvalid_path(s))) for s in filenames]
     batch_filename_tuples = [(s, spellvalid_path(s)) for s in filenames]
+    batch_filename_tuples = [(os.path.join(path, old), os.path.join(
+        path, new)) for (old, new) in batch_filename_tuples]
+
+    print(batch_filename_tuples)
     for (old, new) in batch_filename_tuples:
         os.rename(old, new)
 
-# batch_rename_pdf("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths")
-# batch_rename_pdf("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Dotnet")
 
-spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\computertheory.pdf")
-spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\linearalgebra.pdf")
-spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\algebraessentials.pdf")
+# Main program
+dir_path = input("Path of the folder: ")
+extension = input("Extension you want to rename to: ")
+
+if not os.path.isdir(dir_path):
+    print("{0} is not a directory".format(dir_path))
+    exit()
+if not extension.startswith("."):
+    exit()
+
+sure = input(
+    "Are you sure to rename the file under directory {dir} with extensions {ext} [Y/y]: ".format(dir=dir_path, ext=extension))
+if sure == 'Y' or sure == 'y':
+    batch_rename(dir_path)
+else:
+    print("Operation abort")
