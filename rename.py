9512b7685d9ac71c1378e38ea3c69173057eb95c
jasonlhy <jasonlhy@gmail.com>
2020-05-24 21:44:19 +0800

    Improve the greedy algorihtm

--- rename.py
+++ rename.py
@@ -9,33 +9,31 @@ def spellvalid_path(path: str):
     # Compute the largest spell valid substring start with every character
     # Store the (pattern, (startIdx, endIdx (inclusive)))
     large_tokens = []
-
     end = len(filename)
-    for i in range(0, end):
-        large_token = None
+    i = 0
+    while i < end:
+        # record the greedy information
+        nextI = i + 1
+        greedy_token = None
+
         for j in range(i, end):
             token = filename[i:j+1]
             if len(token) != 0 and h.spell(token):
-                large_token = (token, (i, j))
-        if large_token != None:
-            large_tokens.append(large_token)
+                greedy_token = token
+                nextI = j + 1
 
-    # Find out the largest token with no overlap
-    # If overlap, take the token from the left
-    last_end_inIdx = None
-    valid_tokens = []
-    for [token, [start_idx, end_inIdx]] in large_tokens:
-        if last_end_inIdx:
-            # Only append the token if no overlap
-            if start_idx > last_end_inIdx:
-                valid_tokens.append(token)
-                last_end_inIdx = end_inIdx
-        else:
-            valid_tokens.append(token)
-            last_end_inIdx = end_inIdx
+        if greedy_token != None:
+            large_tokens.append(greedy_token)
+        
+        # If we find the spell greedy token, other next token will look after than token
+        # Otherwise we increment by 1
+        i = nextI
+        
 
-    # print(valid_tokens)
-    valid_filename = "-".join(str(x.strip()) for x in valid_tokens)
+    print(large_tokens)
+    valid_filename = "-".join(str(x.strip()) for x in large_tokens)
+    print("valid_filename: " + valid_filename)
+
     filepath = os.path.join(os.path.dirname(path), valid_filename + extension)
     return filepath
 
@@ -49,4 +47,8 @@ def batch_rename_pdf(path: str):
         os.rename(old, new)
 
 # batch_rename_pdf("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths")
-batch_rename_pdf("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Dotnet")
+# batch_rename_pdf("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Dotnet")
+
+spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\computertheory.pdf")
+spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\linearalgebra.pdf")
+spellvalid_path("C:\\Users\\jason\\OneDrive\\Humble Bundle\\Maths\\algebraessentials.pdf")
